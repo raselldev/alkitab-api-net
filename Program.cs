437729727190b56af908ea08e1c1c4e713ae1baf
raselldev <rrrraka@gmail.com>
2022-11-26 22:46:24 +0700

    testing git client

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+
+//implement interface
 builder.Services.AddSingleton<IBookService, BookService>();
 builder.Services.AddSingleton<IPassageService, PassageService>();
 
